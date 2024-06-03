9d2c5eee51fecbff6ef8255264b6f0e9ea0ac86a
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-06-03 18:33:56 +0200

    Update index.ts

--- server/ts/index.ts
+++ server/ts/index.ts
@@ -6,15 +6,16 @@ import Cipher from './tools/cipher/main.js';
 import { hexToText, textToHex } from './utils/convert.js';
 import { normalizeData } from './utils/data.js';
 
-async function test() {
-    const ciphered = await Cipher.encrypt(textToHex(normalizeData("1(X,,X)2(X,,X)3(X,,X)4!<0~0>!5(X,,X)6(X,,X)7(X,,X)8")), "omgThisIsSoCoolKeyomgThisIsSoCoolKey", "nameomega");
-    const deciphered = await Cipher.decrypt(ciphered, "omgThisIsSoCoolKeyomgThisIsSoCoolKey", "nameomega");
-    console.log(ciphered, hexToText(ciphered));
-    console.log(deciphered, hexToText(deciphered));
-}
-test();
+// async function test() {
+//     const ciphered = await Cipher.encrypt(textToHex(normalizeData("1(X,,X)2(X,,X)3(X,,X)4!<0~0>!5(X,,X)6(X,,X)7(X,,X)8")), "omgThisIsSoCoolKeyomgThisIsSoCoolKey", "nameomega");
+//     const deciphered = await Cipher.decrypt(ciphered, "omgThisIsSoCoolKeyomgThisIsSoCoolKey", "nameomega");
+//     console.log(ciphered, hexToText(ciphered));
+//     console.log(deciphered, hexToText(deciphered));
+// }
+// test();
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.  
+
 if (require('electron-squirrel-startup')) {
     app.quit();
 }
