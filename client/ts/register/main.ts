7be236fa7be3cb5cdfa097d35e9c7839d35e0dfe
Xbat <99105387+MarekSklar@users.noreply.github.com>
2024-06-10 19:55:11 +0200

    Update register

--- client/ts/register/main.ts
+++ client/ts/register/main.ts
@@ -5,21 +5,13 @@ addEventListenerToID();
 addEventListenerToSubmitLoginForm();
 addEventListenerToLoginFormChange();
 
-export async function register(id: string, key: string) {
+export function register(id: string, key: string) {
 
     const dataInText = "";
 
     sessionStorage.setItem("data", dataInText);
     sessionStorage.setItem("id", id);
     sessionStorage.setItem("key", key);
-
-    const blob = new Blob([dataInText], {type: 'application/txt'});
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = url;
-    link.download = `pass-file.txt`;
-    link.click();
-    URL.revokeObjectURL(url);
 
     window.location.href = "table.html";
 }
