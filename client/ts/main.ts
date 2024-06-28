3fb6c3cf5c682ccf18acb4eab2d72331be796c1b
Xbat <99105387+MarekSklar@users.noreply.github.com>
2024-06-28 16:56:16 +0200

    Added loading screen

--- client/ts/main.ts
+++ client/ts/main.ts
@@ -1,3 +1,7 @@
 export function startLoading() {
-    
+    document.getElementById("load-screen").style.display = "flex";
+}
+
+export function endLoading() {
+    document.getElementById("load-screen").style.display = "none";
 }
